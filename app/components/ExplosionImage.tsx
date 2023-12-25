85c795d221d8a3926e39edf5d3585a0ae02517ef
Riddhiman007 <91105040+Ridhiman007@users.noreply.github.com>
2023-12-25 12:02:20 +0530

    1) minor tweaks in the landing 2) light mode needs to be repaired

---
+++ app/components/ExplosionImage.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+import Explosion from "@/explosion.png";
+import { useTheme } from "next-themes";
+
+export default function ExplosionImage() {
+  const { theme } = useTheme();
+  return (
+    <>
+      {theme === "dark" && (
+        <Image
+          className="absolute inset-y-0 right-10 z-10 m-auto self-center opacity-60 mix-blend-color-dodge"
+          src={Explosion}
+          alt="explosion"
+          width={700}
+          height={400}
+        />
+      )}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,27 @@
 import { Box, Container, Typography, duration } from "@mui/material";
 import TypeAnimation from "./components/TypeWriter";
 import React from "react";
-import Explosion from "@/explosion.png";
 import Image from "next/image";
 import MotionDiv from "./components/Motion/MotionDiv";
+import SchoolImage from "./20231212_133837.png";
+import ExplosionImage from "./components/ExplosionImage";
+
 export default function Home() {
   return (
     <>
       <Image
-        className="absolute inset-y-0 right-10 z-10 m-auto self-center dark:mix-blend-color-dodge"
-        src={Explosion}
-        alt="explosion"
-        width={700}
-        height={400}
+        src={SchoolImage}
+        alt="school image"
+        className="absolute inset-0 z-10 opacity-30 mix-blend-color-dodge"
+        fill
       />
+      <ExplosionImage />
       <Box
         component="main"
-        className="absolute inset-0 flex flex-row bg-gradient-to-b dark:from-blue-950 dark:to-slate-950"
+        className="absolute inset-0 flex flex-row bg-red-700/90 bg-none dark:bg-gradient-to-b dark:from-blue-950 dark:to-slate-950"
       >
         <Container
-          className="m-auto ml-0 flex flex-row"
+          className="relative inset-0 ml-0 mt-40 flex flex-row bg-black/60 dark:bg-transparent"
           component={MotionDiv}
           initial={{ opacity: 0, y: 400 }}
           animate={{ opacity: 1, y: 0 }}
@@ -29,7 +31,7 @@ export default function Home() {
             <Box>
               <Typography
                 variant="body2"
-                className="ml-1 text-sm md:text-base lg:text-lg"
+                className="ml-4 text-sm md:text-base lg:text-6xl"
               >
                 We believe in
               </Typography>
