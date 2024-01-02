b93815f6217322b8f756275911c540153dd7b831
Riddhiman007 <91105040+Riddhiman007@users.noreply.github.com>
2024-01-02 18:06:27 +0530

    Update ExplosionImage.tsx

--- app/components/ExplosionImage.tsx
+++ app/components/ExplosionImage.tsx
@@ -1,16 +1,19 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, {useContext} from "react";
 
 import Explosion from "@/explosion.png";
 import { useTheme } from "next-themes";
 import {IsMobileContext} from "@/contexts/IsMobileContext"
 
 export default function ExplosionImage() {
+  const isMobile = useContext(IsMobileContext)
   const { theme } = useTheme();
   return (
     <>
       {theme === "dark" && (
+      <>
+        {!isMobile && (
         <Image
           className="absolute inset-y-0 right-10 z-10 m-auto self-center opacity-60 mix-blend-color-dodge"
           src={Explosion}
@@ -18,6 +21,8 @@ export default function ExplosionImage() {
           width={700}
           height={400}
         />
+      )}
+      </>
       )}
     </>
   );
